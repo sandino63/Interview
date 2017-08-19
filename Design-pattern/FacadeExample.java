8ed193d315ce8a84a131d88dc3833680e4c43e21
sandeep kumar dinodiya <sandino63@gmail.com>
2017-08-19 17:23:09 +0530

    Create FacadeExample.java

---
+++ Design-pattern/FacadeExample.java
@@ -0,0 +1,45 @@
+/* Complex parts */
+
+class CPU {
+    public void freeze() { ... }
+    public void jump(long position) { ... }
+    public void execute() { ... }
+}
+
+class HardDrive {
+    public byte[] read(long lba, int size) { ... }
+}
+
+class Memory {
+    public void load(long position, byte[] data) { ... }
+}
+
+/* Facade */
+
+class ComputerFacade {
+    private CPU processor;
+    private Memory ram;
+    private HardDrive hd;
+
+    public ComputerFacade() {
+        this.processor = new CPU();
+        this.ram = new Memory();
+        this.hd = new HardDrive();
+    }
+
+    public void start() {
+        processor.freeze();
+        ram.load(BOOT_ADDRESS, hd.read(BOOT_SECTOR, SECTOR_SIZE));
+        processor.jump(BOOT_ADDRESS);
+        processor.execute();
+    }
+}
+
+/* Client */
+
+class FacadeExample {
+    public static void main(String[] args) {
+        ComputerFacade computer = new ComputerFacade();
+        computer.start();
+    }
+}
